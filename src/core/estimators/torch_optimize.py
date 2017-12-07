871cc955f5aba717059fc31f24ae9a86349d5666
alexandre_bone <alexandre.bone@icm-institute.org>
2017-12-07 15:05:31 +0100

    Small correction.

--- src/core/estimators/torch_optimize.py
+++ src/core/estimators/torch_optimize.py
@@ -77,12 +77,7 @@ class TorchOptimize(AbstractEstimator):
               Decimal(str(self.CurrentAttachement.data.numpy()[0])),
               Decimal(str(self.CurrentRegularity.data.numpy()[0]))))
 
-<<<<<<< HEAD
-        self.StatisticalModel.SetFixedEffects(fixedEffects)
-        self.StatisticalModel.Write(self.Dataset)
-=======
     # Save the current best results.
     def Write(self):
         self.StatisticalModel.SetFixedEffects(self.BestFixedEffects)
         self.StatisticalModel.Write(self.Dataset)
->>>>>>> d385e3646e2824214e5f905d584ff06e131f77de
