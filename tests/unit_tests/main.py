387d0ecc2bfc59b1081bb47658ca9c1a9dc47515
LOUIS Maxime <maxime.louis@icm-institute.org>
2017-12-14 16:41:36 +0000

    Adding simple tests on point clouds

--- tests/unit_tests/main.py
+++ tests/unit_tests/main.py
@@ -5,7 +5,10 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + '../.
 
 from pydeformetrica.tests.unit_tests.surface_mesh_tests import SurfaceMeshTests
 from pydeformetrica.tests.unit_tests.poly_line_tests import PolyLineTests
+from pydeformetrica.tests.unit_tests.point_cloud_tests import PointCloudTests
+
 from pydeformetrica.tests.unit_tests.in_out_utils_tests import InOutUtilsTests
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(SurfaceMeshTests)
 unittest.TextTestRunner(verbosity=2).run(suite)
@@ -14,4 +17,7 @@ suite = unittest.TestLoader().loadTestsFromTestCase(InOutUtilsTests)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(PolyLineTests)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(PointCloudTests)
 unittest.TextTestRunner(verbosity=2).run(suite)
