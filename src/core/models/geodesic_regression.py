604a67d0f83984108b0e076640fe6c56225dea0f
alexandre_bone <alexandre.bone@icm-institute.org>
2017-12-14 14:38:14 +0100

    Factorizes the initialization of control points.

--- src/core/models/geodesic_regression.py
+++ src/core/models/geodesic_regression.py
@@ -15,6 +15,7 @@ from pydeformetrica.src.in_out.dataset_functions import create_template_metadata
 from pydeformetrica.src.core.model_tools.deformations.geodesic import Geodesic
 from pydeformetrica.src.core.observations.deformable_objects.deformable_multi_object import DeformableMultiObject
 from pydeformetrica.src.support.utilities.general_settings import Settings
+from pydeformetrica.src.support.utilities.initializing_functions import create_regular_grid_of_points
 from pydeformetrica.src.support.kernels.kernel_functions import create_kernel
 from pydeformetrica.src.in_out.utils import *
 from pydeformetrica.src.core.model_tools.attachments.multi_object_attachment import MultiObjectAttachment
@@ -275,45 +276,9 @@ class GeodesicRegression(AbstractStatisticalModel):
         """
         Initialize the control points fixed effect.
         """
-
-        dimension = Settings().Dimension
-
-        axis = []
-        for d in range(dimension):
-            min = self.bounding_box[d, 0]
-            max = self.bounding_box[d, 1]
-            length = max - min
-            assert (length > 0)
-
-            offset = 0.5 * (length - self.initial_cp_spacing * math.floor(length)) / self.initial_cp_spacing
-            axis.append(np.arange(min + offset, max, self.initial_cp_spacing))
-
-        if dimension == 2:
-            x_axis, y_axis = np.meshgrid(axis[0], axis[1])
-
-            assert (x_axis.shape == y_axis.shape)
-            self.number_of_control_points = x_axis.flatten().shape[0]
-            control_points = np.zeros((self.number_of_control_points, dimension))
-
-            control_points[:, 0] = x_axis.flatten()
-            control_points[:, 1] = y_axis.flatten()
-
-        elif dimension == 3:
-            x_axis, y_axis, z_axis = np.meshgrid(axis[0], axis[1], axis[2])
-
-            assert (x_axis.shape == y_axis.shape)
-            assert (x_axis.shape == z_axis.shape)
-            self.number_of_control_points = x_axis.flatten().shape[0]
-            control_points = np.zeros((self.number_of_control_points, dimension))
-
-            control_points[:, 0] = x_axis.flatten()
-            control_points[:, 1] = y_axis.flatten()
-            control_points[:, 2] = z_axis.flatten()
-
-        else:
-            raise RuntimeError('In DeterministicAtlas.Initializecontrol_points: invalid ambient space dimension.')
-
+        control_points = create_regular_grid_of_points(self.bounding_box, self.initial_cp_spacing)
         self.set_control_points(control_points)
+        self.number_of_control_points = control_points.shape[0]
         print('>> Set of ' + str(self.number_of_control_points) + ' control points defined.')
 
     def _initialize_momenta(self):
