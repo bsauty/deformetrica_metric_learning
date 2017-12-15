39e0e6c7ffd57b4e7193c3906718727a4f64a6c7
alexandre_bone <alexandre.bone@icm-institute.org>
2017-12-15 17:19:09 +0100

    New way to launch the software.

--- src/launch/estimate_geodesic_regression.py
+++ src/launch/estimate_geodesic_regression.py
@@ -11,140 +11,113 @@ from pydeformetrica.src.core.models.geodesic_regression import GeodesicRegressio
 from pydeformetrica.src.core.estimators.torch_optimize import TorchOptimize
 from pydeformetrica.src.core.estimators.scipy_optimize import ScipyOptimize
 from pydeformetrica.src.core.estimators.gradient_ascent import GradientAscent
-from pydeformetrica.src.in_out.xml_parameters import XmlParameters
 from pydeformetrica.src.support.utilities.general_settings import Settings
 from pydeformetrica.src.support.kernels.kernel_functions import create_kernel
 from pydeformetrica.src.in_out.dataset_functions import create_dataset
 from src.in_out.utils import *
 
-"""
-Basic info printing.
+def estimate_geodesic_regression(xml_parameters):
 
-"""
+    print('[ estimate_geodesic_regression function ]')
+    print('')
 
-print('')
-print('##############################')
-print('##### PyDeformetrica 1.0 #####')
-print('##############################')
-print('')
+    """
+    Create the dataset object.
+    """
 
-print('[ estimate_geodesic_regression function ]')
-print('')
+    dataset = create_dataset(xml_parameters.dataset_filenames, xml_parameters.visit_ages,
+                             xml_parameters.subject_ids, xml_parameters.template_specifications)
 
-"""
-Read command line, read xml files, set general settings.
+    assert dataset.is_time_series(), "Cannot run a geodesic regression on a non-time_series dataset."
 
-"""
+    """
+    Create the model object.
+    """
 
-assert len(sys.argv) >= 4, "Usage: " + sys.argv[0] + " <model.xml> <data_set.xml> <optimization_parameters.xml>"
-model_xml_path = sys.argv[1]
-dataset_xml_path = sys.argv[2]
-optimization_parameters_xml_path = sys.argv[3]
+    model = GeodesicRegression()
 
-xml_parameters = XmlParameters()
-xml_parameters.read_all_xmls(model_xml_path, dataset_xml_path, optimization_parameters_xml_path)
+    model.diffeomorphism.set_kernel(create_kernel(xml_parameters.deformation_kernel_type,
+                                                  xml_parameters.deformation_kernel_width))
+    model.diffeomorphism.concentration_of_time_points = xml_parameters.number_of_time_points
+    model.diffeomorphism.t0 = xml_parameters.t0
 
-"""
-Create the dataset object.
+    if not xml_parameters.initial_control_points is None:
+        control_points = read_2D_array(xml_parameters.initial_control_points)
+        model.set_control_points(control_points)
 
-"""
+    if not xml_parameters.initial_momenta is None:
+        momenta = read_momenta(xml_parameters.initial_momenta)
+        model.set_momenta(momenta)
 
-dataset = create_dataset(xml_parameters.dataset_filenames, xml_parameters.visit_ages,
-                         xml_parameters.subject_ids, xml_parameters.template_specifications)
+    model.freeze_template = xml_parameters.freeze_template  # this should happen before the init of the template and the cps
+    model.freeze_control_points = xml_parameters.freeze_control_points
 
-assert dataset.is_time_series(), "Cannot run a geodesic regression on a non-time_series dataset."
+    model.initialize_template_attributes(xml_parameters.template_specifications)
 
-"""
-Create the model object.
+    model.use_sobolev_gradient = xml_parameters.use_sobolev_gradient
+    model.smoothing_kernel_width = xml_parameters.deformation_kernel_width * xml_parameters.sobolev_kernel_width_ratio
+    model.initial_cp_spacing = xml_parameters.initial_cp_spacing
+    model.number_of_subjects = dataset.number_of_subjects
 
-"""
+    model.update()
 
-model = GeodesicRegression()
+    """
+    Create the estimator object.
+    """
 
-model.diffeomorphism.set_kernel(create_kernel(xml_parameters.deformation_kernel_type,
-                                              xml_parameters.deformation_kernel_width))
-model.diffeomorphism.concentration_of_time_points = xml_parameters.number_of_time_points
-model.diffeomorphism.t0 = xml_parameters.t0
+    if xml_parameters.optimization_method_type == 'GradientAscent'.lower():
+        estimator = GradientAscent()
+        estimator.initial_step_size = xml_parameters.initial_step_size
+        estimator.max_line_search_iterations = xml_parameters.max_line_search_iterations
+        estimator.line_search_shrink = xml_parameters.line_search_shrink
+        estimator.line_search_expand = xml_parameters.line_search_expand
 
-if not xml_parameters.initial_control_points is None:
-    control_points = read_2D_array(xml_parameters.initial_control_points)
-    model.set_control_points(control_points)
+    elif xml_parameters.optimization_method_type == 'TorchLBFGS'.lower():
+        if not model.freeze_template and model.use_sobolev_gradient:
+            model.use_sobolev_gradient = False
+            msg = 'Impossible to use a Sobolev gradient for the template data with the TorchLBFGS estimator. ' \
+                  'Overriding the "use_sobolev_gradient" option, now set to "off".'
+            warnings.warn(msg)
+        estimator = TorchOptimize()
 
-if not xml_parameters.initial_momenta is None:
-    momenta = read_momenta(xml_parameters.initial_momenta)
-    model.set_momenta(momenta)
+    elif xml_parameters.optimization_method_type == 'ScipyLBFGS'.lower():
+        if not model.freeze_template and model.use_sobolev_gradient:
+            model.use_sobolev_gradient = False
+            msg = 'Impossible to use a Sobolev gradient for the template data with the ScipyLBFGS estimator. ' \
+                  'Overriding the "use_sobolev_gradient" option, now set to "off".'
+            warnings.warn(msg)
+        estimator = ScipyOptimize()
 
-model.freeze_template = xml_parameters.freeze_template  # this should happen before the init of the template and the cps
-model.freeze_control_points = xml_parameters.freeze_control_points
+    else:
+        estimator = GradientAscent()
+        estimator.initial_step_size = xml_parameters.initial_step_size
+        estimator.max_line_search_iterations = xml_parameters.max_line_search_iterations
+        estimator.line_search_shrink = xml_parameters.line_search_shrink
+        estimator.line_search_expand = xml_parameters.line_search_expand
 
-model.initialize_template_attributes(xml_parameters.template_specifications)
-
-model.use_sobolev_gradient = xml_parameters.use_sobolev_gradient
-model.smoothing_kernel_width = xml_parameters.deformation_kernel_width * xml_parameters.sobolev_kernel_width_ratio
-model.initial_cp_spacing = xml_parameters.initial_cp_spacing
-model.number_of_subjects = dataset.number_of_subjects
-
-model.update()
-
-"""
-Create the estimator object.
-
-"""
-
-if xml_parameters.optimization_method_type == 'GradientAscent'.lower():
-    estimator = GradientAscent()
-    estimator.initial_step_size = xml_parameters.initial_step_size
-    estimator.max_line_search_iterations = xml_parameters.max_line_search_iterations
-    estimator.line_search_shrink = xml_parameters.line_search_shrink
-    estimator.line_search_expand = xml_parameters.line_search_expand
-
-elif xml_parameters.optimization_method_type == 'TorchLBFGS'.lower():
-    if not model.freeze_template and model.use_sobolev_gradient:
-        model.use_sobolev_gradient = False
-        msg = 'Impossible to use a Sobolev gradient for the template data with the TorchLBFGS estimator. ' \
-              'Overriding the "use_sobolev_gradient" option, now set to "off".'
+        msg = 'Unknown optimization-method-type: \"' + xml_parameters.optimization_method_type \
+              + '\". Defaulting to GradientAscent.'
         warnings.warn(msg)
-    estimator = TorchOptimize()
 
-elif xml_parameters.optimization_method_type == 'ScipyLBFGS'.lower():
-    if not model.freeze_template and model.use_sobolev_gradient:
-        model.use_sobolev_gradient = False
-        msg = 'Impossible to use a Sobolev gradient for the template data with the ScipyLBFGS estimator. ' \
-              'Overriding the "use_sobolev_gradient" option, now set to "off".'
-        warnings.warn(msg)
-    estimator = ScipyOptimize()
+    estimator.max_iterations = xml_parameters.max_iterations
+    estimator.convergence_tolerance = xml_parameters.convergence_tolerance
 
-else:
-    estimator = GradientAscent()
-    estimator.initial_step_size = xml_parameters.initial_step_size
-    estimator.max_line_search_iterations = xml_parameters.max_line_search_iterations
-    estimator.line_search_shrink = xml_parameters.line_search_shrink
-    estimator.line_search_expand = xml_parameters.line_search_expand
+    estimator.print_every_n_iters = xml_parameters.print_every_n_iters
+    estimator.save_every_n_iters = xml_parameters.save_every_n_iters
 
-    msg = 'Unknown optimization-method-type: \"' + xml_parameters.optimization_method_type \
-          + '\". Defaulting to GradientAscent.'
-    warnings.warn(msg)
+    estimator.dataset = dataset
+    estimator.statistical_model = model
 
-estimator.max_iterations = xml_parameters.max_iterations
-estimator.convergence_tolerance = xml_parameters.convergence_tolerance
+    """
+    Launch.
+    """
 
-estimator.print_every_n_iters = xml_parameters.print_every_n_iters
-estimator.save_every_n_iters = xml_parameters.save_every_n_iters
+    if not os.path.exists(Settings().output_dir):
+        os.makedirs(Settings().output_dir)
 
-estimator.dataset = dataset
-estimator.statistical_model = model
+    model.name = 'geodesic_regression'
 
-"""
-Launch.
-
-"""
-
-if not os.path.exists(Settings().output_dir):
-    os.makedirs(Settings().output_dir)
-
-model.name = 'geodesic_regression'
-
-start_time = time.time()
-estimator.update()
-end_time = time.time()
-print('>> Estimation took: ' + str(time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))))
+    start_time = time.time()
+    estimator.update()
+    end_time = time.time()
+    print('>> Estimation took: ' + str(time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))))
