fb6798d3e87ebb95e78c1f84b1454b3ed0a89ae0
Benoit MARTIN <benoit.martin@icm-institute.org>
2018-05-16 19:15:35 +0200

    generic bench runner for kernels

--- benchmark/kernels.py
+++ benchmark/kernels.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class CudaExactTorch:
-    def __init__(self, tensor_size, tensor_initial_device='cpu'):
+class BenchRunner:
+    def __init__(self, kernel, tensor_size, tensor_initial_device='cpu'):
         # tensor_size = (4, 3)
-        # print('CudaExactTorch::__init()__ getting kernel and initializing tensors with size ' + str(tensor_size))
-        self.kernel_instance = kernel_factory.factory(kernel_factory.Type.CUDA_EXACT_TORCH, kernel_width=1.)
+        # print('BenchRunner::__init()__ getting kernel and initializing tensors with size ' + str(tensor_size))
+        self.kernel_instance = kernel_factory.factory(kernel, kernel_width=1.)
 
         self.x = torch.rand(tensor_size, device=torch.device(tensor_initial_device))
         self.y = self.x.clone()
@@ -16,7 +16,7 @@ class CudaExactTorch:
 
         # run once for warm-up: cuda pre-compile
         self.res = self.kernel_instance.convolve(self.x, self.y, self.p)
-        # print('CudaExactTorch::__init()__ done')
+        # print('BenchRunner::__init()__ done')
 
     def run(self):
         self.res = self.kernel_instance.convolve(self.x, self.y, self.p)
@@ -29,20 +29,22 @@ class CudaExactTorch:
         # torch.cuda.empty_cache()
 
     def __exit__(self):
-        print('CudaExactTorch::__exit()__')
+        print('BenchRunner::__exit()__')
 
 
 def build_setup():
-    kernels = ['CudaExactTorch']
-    initial_devices = ['cpu', 'cuda:0']
+    # kernels = ['CudaExactTorch']
+    kernels = ['exact']
+    # initial_devices = ['cpu', 'cuda:0']
+    initial_devices = ['cpu']
     tensor_sizes = [(4, 3), (16, 3), (32, 3), (64, 3), (128, 3), (256, 3), (512, 3)]
     # tensor_sizes = [(4, 3), (16, 3), (32, 3)]
     setups = []
 
     for k, d, t in [(k, d, t) for k in kernels for d in initial_devices for t in tensor_sizes]:
         bench_setup = '''
-from __main__ import CudaExactTorch
-instance = {kernel}({tensor}, '{device}')
+from __main__ import BenchRunner
+bench = BenchRunner('{kernel}', {tensor}, '{device}')
             '''.format(kernel=k, tensor=str(t), device=d)
 
         setups.append({'kernel': k, 'device': d, 'tensor_size': t, 'bench_setup': bench_setup})
@@ -59,29 +61,32 @@ if __name__ == "__main__":
 
         res = {}
         res['setup'] = setup
-        res['data'] = timeit.repeat("instance.run()", number=1000, repeat=3, setup=setup['bench_setup'])
+        res['data'] = timeit.repeat("bench.run()", number=1000, repeat=3, setup=setup['bench_setup'])
         res['min'] = min(res['data'])
         res['max'] = max(res['data'])
 
         print(res)
         results.append(res)
 
-    # print('cpu: ' + str(timeit.repeat("instance.run()", number=50000, repeat=3, setup=setup_cpu)))
-    # print('cuda: ' + str(timeit.repeat("instance.run()", number=50000, repeat=3, setup=setup_cuda)))
+    # print('cpu: ' + str(timeit.repeat("bench.run()", number=50000, repeat=3, setup=setup_cpu)))
+    # print('cuda: ' + str(timeit.repeat("bench.run()", number=50000, repeat=3, setup=setup_cuda)))
 
-    cpu_res = [r['max'] for r in results if r['setup']['device'] == 'cpu']
-    cuda_res = [r['max'] for r in results if r['setup']['device'] == 'cuda:0']
-    assert(len(cpu_res) == len(cuda_res))
+    # cpu_res = [r['max'] for r in results if r['setup']['device'] == 'cpu']
+    # cuda_res = [r['max'] for r in results if r['setup']['device'] == 'cuda:0']
+    # assert(len(cpu_res) == len(cuda_res))
 
     fig, ax = plt.subplots()
     ax.set_yscale('log', nonposy='clip')
 
-    index = np.arange(len(cpu_res))
-    bar_width = 0.35
-    opacity = 0.4
+    for d in set([r['setup']['device'] for r in results]):
+        res_data = [r['max'] for r in results if r['setup']['device'] == d]
+        index = np.arange(len(res_data))
+        bar_width = 0.35
+        opacity = 0.4
+        ax.bar(index, res_data, bar_width, alpha=opacity, label=d)
 
-    bar1 = ax.bar(index, cpu_res, bar_width, alpha=opacity, color='b', label='cpu')
-    bar2 = ax.bar(index + bar_width, cuda_res, bar_width, alpha=opacity, color='g', label='cuda')
+    # bar1 = ax.bar(index, cpu_res, bar_width, alpha=opacity, color='b', label='cpu')
+    # bar2 = ax.bar(index + bar_width, cuda_res, bar_width, alpha=opacity, color='g', label='cuda')
 
     ax.set_xlabel('Tensor size')
     ax.set_ylabel('Runtime (s)')
