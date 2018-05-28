220094181bd215560b7f03111b41f91906a1f7d1
Benoit MARTIN <benoit.martin@icm-institute.org>
2018-05-28 16:50:19 +0200

    replace pre-defined tensors with rand tensor and manual seed

--- tests/unit_tests/kernel_factory_tests.py
+++ tests/unit_tests/kernel_factory_tests.py
@@ -53,23 +53,23 @@ class KernelTestBase(unittest.TestCase):
     def setUp(self):
         Settings().dimension = 3
 
-        self.x = torch.tensor([
-            [1., 1., 1.],
-            [1., 1., 1.],
-            [1., 1., 1.],
-            [1., 1., 1.]])
-        self.y = self.x.clone()
-        self.p = torch.ones([4, Settings().dimension])
+        torch.manual_seed(42)  # for reproducibility
+        torch.set_printoptions(precision=30)    # for more precision when printing tensor
+
+        self.x = torch.rand([4, 3])
+        self.y = torch.rand([4, 3])
+        self.p = torch.rand([4, 3])
         self.expected_convolve_res = torch.tensor([
-            [4., 4., 4.],
-            [4., 4., 4.],
-            [4., 4., 4.],
-            [4., 4., 4.]])
+            [1.098455786705017089843750000000, 0.841387629508972167968750000000, 1.207388281822204589843750000000],
+            [1.135044455528259277343750000000, 0.859343230724334716796875000000, 1.387768864631652832031250000000],
+            [1.258846044540405273437500000000, 0.927951514720916748046875000000, 1.383145928382873535156250000000],
+            [1.334064722061157226562500000000, 0.887639760971069335937500000000, 1.360100984573364257812500000000]])
+
         self.expected_convolve_gradient_res = torch.tensor([
-            [0., 0., 0.],
-            [0., 0., 0.],
-            [0., 0., 0.],
-            [0., 0., 0.]])
+            [-1.623382568359375000000000000000, -1.212645769119262695312500000000, 1.440739274024963378906250000000],
+            [-1.414733767509460449218750000000, 1.848072409629821777343750000000, -0.102501690387725830078125000000],
+            [1.248104929924011230468750000000, 0.059575259685516357421875000000, -1.860013246536254882812500000000],
+            [1.790011405944824218750000000000, -0.695001959800720214843750000000, 0.521775603294372558593750000000]])
 
         super().setUp()
 
@@ -89,9 +89,9 @@ class Kernel(KernelTestBase):
         res = res.to(torch.device('cpu'))
         # torch.set_printoptions(precision=25)
         # print(res)
-        # print(expected_convolve_res)
-        # print(res - expected_convolve_res)
-        self.assertTrue(torch.equal(self.expected_convolve_res, res))
+        # print(self.expected_convolve_res)
+        # print(res - self.expected_convolve_res)
+        self.assertTrue(torch.equal(self.expected_convolve_res, res), 'convolve did not produce expected result')
 
         # test convolve gradient method
         res = self.kernel_instance.convolve_gradient(self.x, self.x)
@@ -99,7 +99,7 @@ class Kernel(KernelTestBase):
         # move to CPU
         res = res.to(torch.device('cpu'))
         # print(res)
-        self.assertTrue(torch.equal(self.expected_convolve_gradient_res, res))
+        self.assertTrue(torch.equal(self.expected_convolve_gradient_res, res), 'convolve_gradient did not produce expected result')
 
     def test_torch_cuda_without_move_to_device(self):
         res = self.kernel_instance.convolve(self.x, self.y, self.p)
@@ -110,7 +110,7 @@ class Kernel(KernelTestBase):
         # print(res)
         # print(expected_convolve_res)
         # print(res - expected_convolve_res)
-        self.assertTrue(torch.equal(self.expected_convolve_res, res))
+        self.assertTrue(torch.equal(self.expected_convolve_res, res), 'convolve did not produce expected result')
 
         # test convolve gradient method
         res = self.kernel_instance.convolve_gradient(self.x, self.x)
@@ -118,7 +118,7 @@ class Kernel(KernelTestBase):
         # move to CPU
         res = res.to(torch.device('cpu'))
         # print(res)
-        self.assertTrue(torch.equal(self.expected_convolve_gradient_res, res))
+        self.assertTrue(torch.equal(self.expected_convolve_gradient_res, res), 'convolve_gradient did not produce expected result')
 
 
 class KeopsKernel(KernelTestBase):
@@ -128,11 +128,11 @@ class KeopsKernel(KernelTestBase):
 
     def test_convolve(self):
         res = self.kernel_instance.convolve(self.x, self.y, self.p)
-        self.assertTrue(torch.equal(self.expected_convolve_res, res))
+        self.assertTrue(torch.equal(self.expected_convolve_res, res), 'convolve did not produce expected result')
 
     def test_convolve_gradient(self):
         res = self.kernel_instance.convolve_gradient(self.x, self.x)
         print(res)
         print(self.expected_convolve_gradient_res)
-        self.assertTrue(torch.equal(self.expected_convolve_gradient_res, res), 'result from convolve_gradient is wrong')
+        self.assertTrue(torch.equal(self.expected_convolve_gradient_res, res), 'convolve_gradient did not produce expected result')
 
