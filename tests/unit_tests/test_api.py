623843307918bc26113a3cf8b75dd0ed9e91bee0
alexandre_bone <alexandre.bone@icm-institute.org>
2018-07-24 16:27:53 +0200

    Registration API works.

--- tests/unit_tests/test_api.py
+++ tests/unit_tests/test_api.py
@@ -277,151 +277,140 @@ class API(unittest.TestCase):
         #                                             freeze_scaling_ratios=True)
         #
 
-    # Regression
+    # # Regression
+    #
+    # def test_estimate_geodesic_regression_landmark_2d_skulls(self):
+    #     dataset_specifications = {
+    #         'dataset_filenames': [
+    #             [{'skull': '../../examples/regression/landmark/2d/skulls/data/skull_australopithecus.vtk'},
+    #              {'skull': '../../examples/regression/landmark/2d/skulls/data/skull_habilis.vtk'},
+    #              {'skull': '../../examples/regression/landmark/2d/skulls/data/skull_erectus.vtk'},
+    #              {'skull': '../../examples/regression/landmark/2d/skulls/data/skull_sapiens.vtk'}]],
+    #         'visit_ages': [[1, 2, 3, 4]],
+    #         'subject_ids': [['australopithecus', 'habilis', 'erectus', 'sapiens']]
+    #     }
+    #     template_specifications = {
+    #         'skull': {'deformable_object_type': 'polyline',
+    #                   'kernel_type': 'torch', 'kernel_width': 20.0,
+    #                   'noise_std': 1.0,
+    #                   'filename': '../../examples/regression/landmark/2d/skulls/data/template.vtk',
+    #                   'attachment_type': 'varifold'}}
+    #
+    #     self.deformetrica.estimate_geodesic_regression(
+    #         template_specifications, dataset_specifications,
+    #         estimator_options={'optimization_method_type': 'GradientAscent', 'max_iterations': 100},
+    #         model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 25.0,
+    #                        'concentration_of_time_points': 5, 'smoothing_kernel_width': 20.0})
+    #
+    # def test_estimate_geodesic_regression_landmark_3d_surprise(self):
+    #     dataset_specifications = {
+    #         'dataset_filenames': [
+    #             [{'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-000.vtk'},
+    #              {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-005.vtk'},
+    #              {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-010.vtk'},
+    #              {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-015.vtk'},
+    #              {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-020.vtk'},
+    #              {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-025.vtk'},
+    #              {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-030.vtk'},
+    #              {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-035.vtk'}]],
+    #         'visit_ages': [[0, 5, 10, 15, 20, 25, 30, 35]],
+    #         'subject_ids': [['ses-000', 'ses-005', 'ses-010', 'ses-015', 'ses-020', 'ses-025', 'ses-030', 'ses-035']]
+    #     }
+    #     template_specifications = {
+    #         'skull': {'deformable_object_type': 'polyline',
+    #                   'noise_std': 0.0035,
+    #                   'filename': '../../examples/regression/landmark/3d/surprise/data/ForInitialization__Template__FromUser.vtk',
+    #                   'attachment_type': 'landmark'}}
+    #
+    #     self.deformetrica.estimate_geodesic_regression(
+    #         template_specifications, dataset_specifications,
+    #         estimator_options={'optimization_method_type': 'GradientAscent', 'max_iterations': 50,
+    #                            'convergence_tolerance': 1e-5, 'initial_step_size': 1e-6},
+    #         model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 0.015,
+    #                        'concentration_of_time_points': 1, 'smoothing_kernel_width': 20.0, 't0': 5.5,
+    #                        'use_sobolev_gradient': True, 'dense_mode': True})
+    #
+    # def test_estimate_geodesic_regression_image_2d_cross(self):
+    #     dataset_specifications = {
+    #         'dataset_filenames': [[{'skull': '../../examples/regression/image/2d/cross/data/cross_-5.png'},
+    #                                 {'skull': '../../examples/regression/image/2d/cross/data/cross_-3.png'},
+    #                                 {'skull': '../../examples/regression/image/2d/cross/data/cross_-2.png'},
+    #                                 {'skull': '../../examples/regression/image/2d/cross/data/cross_0.png'},
+    #                                 {'skull': '../../examples/regression/image/2d/cross/data/cross_1.png'},
+    #                                 {'skull': '../../examples/regression/image/2d/cross/data/cross_3.png'}]],
+    #         'visit_ages': [[-5, -3, -2, 0, 1, 3]],
+    #         'subject_ids': [['t-5', 't-3', 't-2', 't0', 't1', 't3']]
+    #     }
+    #     template_specifications = {
+    #         'skull': {'deformable_object_type': 'image',
+    #                   'noise_std': 0.1,
+    #                   'filename': '../../examples/regression/image/2d/cross/data/cross_0.png',
+    #                   'attachment_type': 'varifold'}}
+    #
+    #     self.deformetrica.estimate_geodesic_regression(
+    #         template_specifications, dataset_specifications,
+    #         estimator_options={'optimization_method_type': 'GradientAscent', 'max_iterations': 50,
+    #                            'initial_step_size': 1e-9},
+    #         model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 10.0,
+    #                        'concentration_of_time_points': 3, 'freeze_template': True})
 
-    def test_estimate_geodesic_regression_landmark_2d_skulls(self):
-        dataset_specifications = {
-            'dataset_filenames': [
-                [{'skull': '../../examples/regression/landmark/2d/skulls/data/skull_australopithecus.vtk'},
-                 {'skull': '../../examples/regression/landmark/2d/skulls/data/skull_habilis.vtk'},
-                 {'skull': '../../examples/regression/landmark/2d/skulls/data/skull_erectus.vtk'},
-                 {'skull': '../../examples/regression/landmark/2d/skulls/data/skull_sapiens.vtk'}]],
-            'visit_ages': [[1, 2, 3, 4]],
-            'subject_ids': [['australopithecus', 'habilis', 'erectus', 'sapiens']]
-        }
-        template_specifications = {
-            'skull': {'deformable_object_type': 'polyline',
-                      'kernel_type': 'torch', 'kernel_width': 20.0,
-                      'noise_std': 1.0,
-                      'filename': '../../examples/regression/landmark/2d/skulls/data/template.vtk',
-                      'attachment_type': 'varifold'}}
+    # Registration
 
-        self.deformetrica.estimate_geodesic_regression(
-            template_specifications, dataset_specifications,
-            estimator_options={'optimization_method_type': 'GradientAscent', 'max_iterations': 100},
-            model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 25.0,
-                           'concentration_of_time_points': 5, 'smoothing_kernel_width': 20.0})
+    # def test_estimate_deterministic_registration_landmark_2d_points(self):
+    #     dataset_specifications = {
+    #         'dataset_filenames': [
+    #             [{'pointcloud': '../../examples/registration/landmark/2d/points/data/target_points.vtk'}]],
+    #         'subject_ids': ['target']
+    #     }
+    #     template_specifications = {
+    #         'pointcloud': {'deformable_object_type': 'landmark',
+    #                        'noise_std': 1e-3,
+    #                        'filename': '../../examples/registration/landmark/2d/points/data/source_points.vtk'}}
+    #
+    #     self.deformetrica.estimate_deterministic_atlas(
+    #         template_specifications, dataset_specifications,
+    #         estimator_options={'optimization_method_type': 'GradientAscent', 'initial_step_size': 1e-8,
+    #                            'max_iterations': 100, 'max_line_search_iterations': 200},
+    #         model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 3.0,
+    #                        'number_of_time_points': 10, 'freeze_template': True, 'freeze_control_points': True})
+    #
+    # def test_estimate_deterministic_registration_landmark_2d_starfish(self):
+    #     dataset_specifications = {
+    #         'dataset_filenames': [
+    #             [{'starfish': '../../examples/registration/landmark/2d/starfish/data/starfish_target.vtk'}]],
+    #         'subject_ids': ['target']
+    #     }
+    #     template_specifications = {
+    #         'starfish': {'deformable_object_type': 'polyline',
+    #                      'kernel_type': 'torch', 'kernel_width': 50.0,
+    #                      'noise_std': 0.1,
+    #                      'attachment_type': 'current',
+    #                      'filename': '../../examples/registration/landmark/2d/starfish/data/starfish_reference.vtk'}}
+    #
+    #     self.deformetrica.estimate_deterministic_atlas(
+    #         template_specifications, dataset_specifications,
+    #         estimator_options={'optimization_method_type': 'ScipyLBFGS', 'max_iterations': 200},
+    #         model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 30.0,
+    #                        'number_of_time_points': 10, 'freeze_template': True, 'freeze_control_points': True})
+    #
+    # def test_estimate_deterministic_registration_image_2d_tetris(self):
+    #     dataset_specifications = {
+    #         'dataset_filenames': [
+    #             [{'image': '../../examples/registration/image/2d/tetris/data/image2.png'}]],
+    #         'subject_ids': ['target']
+    #     }
+    #     template_specifications = {
+    #         'image': {'deformable_object_type': 'image',
+    #                   'kernel_type': 'torch',
+    #                   'kernel_width': 10.0,
+    #                   'noise_std': 0.1,
+    #                   'filename': '../../examples/registration/image/2d/tetris/data/image1.png'}}
+    #
+    #     self.deformetrica.estimate_deterministic_atlas(
+    #         template_specifications, dataset_specifications,
+    #         estimator_options={'optimization_method_type': 'GradientAscent'},
+    #         model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 20.0})
 
-    def test_estimate_geodesic_regression_landmark_3d_surprise(self):
-        dataset_specifications = {
-            'dataset_filenames': [
-                [{'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-000.vtk'},
-                 {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-005.vtk'},
-                 {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-010.vtk'},
-                 {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-015.vtk'},
-                 {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-020.vtk'},
-                 {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-025.vtk'},
-                 {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-030.vtk'},
-                 {'skull': '../../examples/regression/landmark/3d/surprise/data/sub-F001_ses-035.vtk'}]],
-            'visit_ages': [[0, 5, 10, 15, 20, 25, 30, 35]],
-            'subject_ids': [['ses-000', 'ses-005', 'ses-010', 'ses-015', 'ses-020', 'ses-025', 'ses-030', 'ses-035']]
-        }
-        template_specifications = {
-            'skull': {'deformable_object_type': 'polyline',
-                      'noise_std': 0.0035,
-                      'filename': '../../examples/regression/landmark/3d/surprise/data/ForInitialization__Template__FromUser.vtk',
-                      'attachment_type': 'landmark'}}
-
-        self.deformetrica.estimate_geodesic_regression(
-            template_specifications, dataset_specifications,
-            estimator_options={'optimization_method_type': 'GradientAscent', 'max_iterations': 50,
-                               'convergence_tolerance': 1e-5, 'initial_step_size': 1e-6},
-            model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 0.015,
-                           'concentration_of_time_points': 1, 'smoothing_kernel_width': 20.0, 't0': 5.5,
-                           'use_sobolev_gradient': True, 'dense_mode': True})
-
-    def test_estimate_geodesic_regression_image_2d_cross(self):
-        dataset_specifications = {
-            'dataset_filenames': [[{'skull': '../../examples/regression/image/2d/cross/data/cross_-5.png'},
-                                    {'skull': '../../examples/regression/image/2d/cross/data/cross_-3.png'},
-                                    {'skull': '../../examples/regression/image/2d/cross/data/cross_-2.png'},
-                                    {'skull': '../../examples/regression/image/2d/cross/data/cross_0.png'},
-                                    {'skull': '../../examples/regression/image/2d/cross/data/cross_1.png'},
-                                    {'skull': '../../examples/regression/image/2d/cross/data/cross_3.png'}]],
-            'visit_ages': [[-5, -3, -2, 0, 1, 3]],
-            'subject_ids': [['t-5', 't-3', 't-2', 't0', 't1', 't3']]
-        }
-        template_specifications = {
-            'skull': {'deformable_object_type': 'image',
-                      'noise_std': 0.1,
-                      'filename': '../../examples/regression/image/2d/cross/data/cross_0.png',
-                      'attachment_type': 'varifold'}}
-
-        self.deformetrica.estimate_geodesic_regression(
-            template_specifications, dataset_specifications,
-            estimator_options={'optimization_method_type': 'GradientAscent', 'max_iterations': 50,
-                               'initial_step_size': 1e-9},
-            model_options={'deformation_kernel_type': 'torch', 'deformation_kernel_width': 10.0,
-                           'concentration_of_time_points': 3, 'freeze_template': True})
-
-        # # Registration
-        #
-        # def test_estimate_deterministic_registration_landmark_2d_points(self):
-        #     dataset_file_names = [[{'pointcloud': '../../examples/registration/landmark/2d/points/data/target_points.vtk'}]]
-        #     visit_ages = []
-        #     subject_ids = ['target']
-        #     template_specifications = {
-        #         'pointcloud': {'deformable_object_type': 'landmark',
-        #                        'noise_std': 1e-3,
-        #                        'filename': '../../examples/registration/landmark/2d/points/data/source_points.vtk'}}
-        #
-        #     dataset = create_dataset(visit_ages, template_specifications, dataset_file_names=dataset_file_names,
-        #                              subject_ids=subject_ids,
-        #                              dimension=2, tensor_scalar_type=torch.DoubleTensor)
-        #
-        #     self.deformetrica.estimate_deterministic_atlas(template_specifications, dataset,
-        #                                                    estimator=GradientAscent,
-        #                                                    estimator_options={'initial_step_size': 1e-8,
-        #                                                                       'max_iterations': 100,
-        #                                                                       'max_line_search_iterations': 200},
-        #                                                    deformation_kernel=kernel_factory.factory(
-        #                                                        kernel_factory.Type.TORCH, kernel_width=3.0),
-        #                                                    number_of_time_points=10, freeze_template=True,
-        #                                                    freeze_control_points=True)
-        #
-        # def test_estimate_deterministic_registration_landmark_2d_starfish(self):
-        #     dataset_file_names = [
-        #         [{'starfish': '../../examples/registration/landmark/2d/starfish/data/starfish_target.vtk'}]]
-        #     visit_ages = []
-        #     subject_ids = ['target']
-        #     template_specifications = {
-        #         'starfish': {'deformable_object_type': 'polyline',
-        #                      'kernel': kernel_factory.factory(kernel_factory.Type.TORCH, kernel_width=50.0),
-        #                      'noise_std': 0.1,
-        #                      'attachment_type': 'current',
-        #                      'filename': '../../examples/registration/landmark/2d/starfish/data/starfish_reference.vtk'}}
-        #
-        #     dataset = create_dataset(visit_ages, template_specifications, dataset_file_names=dataset_file_names,
-        #                              subject_ids=subject_ids,
-        #                              dimension=2, tensor_scalar_type=torch.DoubleTensor)
-        #
-        #     self.deformetrica.estimate_deterministic_atlas(template_specifications, dataset,
-        #                                                    estimator=ScipyOptimize,
-        #                                                    estimator_options={'max_iterations': 200},
-        #                                                    deformation_kernel=kernel_factory.factory(
-        #                                                        kernel_factory.Type.TORCH, kernel_width=30.0),
-        #                                                    number_of_time_points=10, freeze_template=True,
-        #                                                    freeze_control_points=True)
-        #
-        # def test_estimate_deterministic_registration_image_2d_tetris(self):
-        #     dataset_file_names = [[{'image': '../../examples/registration/image/2d/tetris/data/image2.png'}]]
-        #     visit_ages = []
-        #     subject_ids = ['target']
-        #     template_specifications = {
-        #         'image': {'deformable_object_type': 'image',
-        #                   'kernel': kernel_factory.factory(kernel_factory.Type.TORCH, kernel_width=10.0),
-        #                   'noise_std': 0.1,
-        #                   'filename': '../../examples/registration/image/2d/tetris/data/image1.png'}}
-        #
-        #     dataset = create_dataset(visit_ages, template_specifications, dataset_file_names=dataset_file_names,
-        #                              subject_ids=subject_ids,
-        #                              dimension=2, tensor_scalar_type=torch.DoubleTensor)
-        #
-        #     self.deformetrica.estimate_deterministic_atlas(template_specifications, dataset,
-        #                                                    estimator=GradientAscent,
-        #                                                    deformation_kernel=kernel_factory.factory(
-        #                                                        kernel_factory.Type.TORCH, kernel_width=20.0))
-        #
         # # Parallel Transport
         #
         # def test_compute_parallel_transport_image_2d_snowman(self):
