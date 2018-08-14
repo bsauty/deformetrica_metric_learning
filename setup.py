b0117a164f9e0a085bafb5126aba1956473b6a81
Benoit MARTIN <benoit.martin@icm-institute.org>
2018-08-14 15:24:19 +0200

    add BUILD_GUI environment variable option to setup.py

--- setup.py
+++ setup.py
@@ -1,46 +1,115 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+import os
+from glob import glob
+from os.path import splitext, basename
 
 from setuptools import setup, find_packages
 
 try:  # for pip >= 10
     from pip._internal.req import parse_requirements
-except ImportError:     # for pip <= 9.0.3
+except ImportError:  # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 
-setup(
-    name='Deformetrica',
-    version=open('VERSION', encoding='utf-8').read(),
-    url='http://www.deformetrica.org',
-    description='Software for the statistical analysis of 2D and 3D shape data.',
-    long_description=open('README.md', encoding='utf-8').read(),
-    author='ARAMIS Lab',
-    maintainer='Deformetrica developers',
-    maintainer_email='deformetrica.team@gmail.com',
-    license='INRIA license',
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    package_data={'': ['*.json', '*.png']},
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'console_scripts': ['deformetrica=deformetrica:main'],  # CLI
-        'gui_scripts': ['deformetrica-gui=gui.__main__:main']   # GUI
-    },
-    classifiers=[
-        'Framework :: Deformetrica',
-        'Development Status :: 4.0.1',
-        'Environment :: Console',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ],
-    install_requires=[
-        'cmake>=3.10',
-        'numpy>=1.10',
-        'gputil>=1.3',
-        'pykeops==0.0.10'
-    ]
-)
+def str_to_bool(s):
+    if s is None:
+        return False
+
+    assert isinstance(s, str), 'given argument must be a string'
+    if s.lower() in ['true', '1', 'yes']:
+        return True
+    elif s.lower() in ['false', '0', 'no']:
+        return False
+    else:
+        raise LookupError
+
+
+def build_deformetrica():
+    print('build_deformetrica()')
+    setup(
+        name='deformetrica',
+        version=open('VERSION', encoding='utf-8').read(),
+        url='http://www.deformetrica.org',
+        description='Software for the statistical analysis of 2D and 3D shape data.',
+        long_description=open('README.md', encoding='utf-8').read(),
+        author='ARAMIS Lab',
+        maintainer='Deformetrica developers',
+        maintainer_email='deformetrica.team@gmail.com',
+        license='INRIA license',
+        package_dir={'': 'src'},
+        packages=find_packages('src', exclude=['gui']),  # exclude gui
+        py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+        # package_data={'': ['*.json', '*.png']},
+        include_package_data=True,
+        zip_safe=False,
+        entry_points={
+            'console_scripts': ['deformetrica=deformetrica:main'],  # CLI
+        },
+        classifiers=[
+            'Framework :: Deformetrica',
+            'Development Status :: 4.0.1',
+            'Environment :: Console',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+        ],
+        install_requires=[
+            'cmake>=3.10',
+            'numpy>=1.10',
+            'gputil>=1.3',
+            'pykeops==0.0.10'
+        ]
+    )
+
+
+def build_deformetrica_and_gui():
+    print('build_deformetrica_and_gui()')
+    setup(
+        name='deformetrica',
+        version=open('VERSION', encoding='utf-8').read(),
+        url='http://www.deformetrica.org',
+        description='Software for the statistical analysis of 2D and 3D shape data.',
+        long_description=open('README.md', encoding='utf-8').read(),
+        author='ARAMIS Lab',
+        maintainer='Deformetrica developers',
+        maintainer_email='deformetrica.team@gmail.com',
+        license='INRIA license',
+        package_dir={'': 'src'},
+        packages=find_packages('src'),
+        py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+        package_data={'': ['*.json', '*.png']},
+        include_package_data=True,
+        zip_safe=False,
+        entry_points={
+            'console_scripts': ['deformetrica=deformetrica:main'],  # CLI
+            'gui_scripts': ['deformetrica-gui=gui.__main__:main']  # GUI
+        },
+        classifiers=[
+            'Framework :: Deformetrica',
+            'Development Status :: 4.0.1',
+            'Environment :: Console',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+        ],
+        install_requires=[
+            'cmake>=3.10',
+            'numpy>=1.10',
+            'gputil>=1.3',
+            'pykeops==0.0.10',
+            'PyQt5>=5.11'
+        ]
+    )
+
+
+# build gui by default
+build_gui = str_to_bool(os.environ['BUILD_GUI']) if 'BUILD_GUI' in os.environ else True
+print('BUILD_GUI=' + str(build_gui))
+
+if build_gui:
+    build_deformetrica_and_gui()
+else:
+    build_deformetrica()
