Fix sign of likelihood

deformetrica/core/estimators/mcmc_saem.py
@@ -127,22 +127,24 @@ class McmcSaem(AbstractEstimator):
         averaged_population_RER = {key: np.zeros(value.shape) for key, value in self.population_RER.items()}
         averaged_individual_RER = {key: np.zeros(value.shape) for key, value in self.individual_RER.items()}
 
+
         # Main loop ----------------------------------------------------------------------------------------------------
         while self.callback_ret and self.current_iteration < self.max_iterations:
             self.current_iteration += 1
             step = self._compute_step_size()
-
+            print("Step for this iteration : ", step)
             # Simulation.
             current_model_terms = None
             for n in tqdm(range(self.sample_every_n_mcmc_iters)):
                 self.current_mcmc_iteration += 1
 
-                # Single iteration of the MCMC.
-                #print(n, time.time())
+                # Single iteration of the MCMC
+                print("Computing acceptance rates and current likelihood with one sampling step ")
+                print("sampling", time.time())
                 self.current_acceptance_rates, current_model_terms = self.sampler.sample(
                     self.statistical_model, self.dataset, self.population_RER, self.individual_RER,
                     current_model_terms)
-                #print(n,time.time())
+                print("sampling", time.time())
 
                 # Adapt proposal variances.
                 self._update_acceptance_rate_information()
@@ -155,13 +157,14 @@ class McmcSaem(AbstractEstimator):
                         self.current_mcmc_iteration,
                         not self.current_iteration % self.print_every_n_iters and n == self.sample_every_n_mcmc_iters - 1)
 
+            print("class1", time.time())
             # Maximization for the class 1 fixed effects.
             sufficient_statistics = self.statistical_model.compute_sufficient_statistics(
                 self.dataset, self.population_RER, self.individual_RER, model_terms=current_model_terms)
             self.sufficient_statistics = {key: value + step * (sufficient_statistics[key] - value) for key, value in
                                           self.sufficient_statistics.items()}
             self.statistical_model.update_fixed_effects(self.dataset, self.sufficient_statistics)
-
+            print("class2", time.time())
             # Maximization for the class 2 fixed effects.
             fixed_effects_before_maximization = self.statistical_model.get_fixed_effects()
             self._maximize_over_fixed_effects()
@@ -169,6 +172,7 @@ class McmcSaem(AbstractEstimator):
             fixed_effects = {key: value + step * (fixed_effects_after_maximization[key] - value) for key, value in
                              fixed_effects_before_maximization.items()}
             self.statistical_model.set_fixed_effects(fixed_effects)
+            print("Done with fixed effects update", time.time())
 
             # Averages the random effect realizations in the concentration phase.
             if step < 1.0:
@@ -278,8 +282,8 @@ class McmcSaem(AbstractEstimator):
                 logger.info('')
                 logger.info('[ end of the gradient-based maximization ]')
 
-        # if self.current_iteration < self.number_of_burn_in_iterations:
-        #     self.statistical_model.preoptimize(self.dataset, self.individual_RER)
+        #if self.current_iteration < self.number_of_burn_in_iterations:
+        #    self.statistical_model.preoptimize(self.dataset, self.individual_RER)
 
     ####################################################################################################################
     ### Other private methods:

deformetrica/core/models/longitudinal_metric_learning.py
@@ -3,6 +3,7 @@ import os.path
 import shutil
 import warnings
 from copy import deepcopy
+import time
 
 import matplotlib.pyplot as plt
 
@@ -226,8 +227,11 @@ class LongitudinalMetricLearning(AbstractStatisticalModel):
         v0, p0, metric_parameters, modulation_matrix = self._fixed_effects_to_torch_tensors(with_grad)
         onset_ages, log_accelerations, sources = self._individual_RER_to_torch_tensors(individual_RER, with_grad)
 
+        print("Beginning _compute_residuals : ", time.time())
         residuals = self._compute_residuals(dataset, v0, p0, metric_parameters, modulation_matrix,
                                             log_accelerations, onset_ages, sources, with_grad=with_grad)
+
+        print("End _compute_residuals : ", time.time())
 
         if mode == 'complete':
             sufficient_statistics = self.compute_sufficient_statistics(dataset, population_RER, individual_RER, residuals)
@@ -236,19 +240,29 @@ class LongitudinalMetricLearning(AbstractStatisticalModel):
         attachments = self._compute_individual_attachments(residuals)
         attachment = torch.sum(attachments)
 
-        regularity = self._compute_random_effects_regularity(log_accelerations, onset_ages, sources)
+        if mode != 'model':
+            rer_reg = self._compute_random_effects_regularity(log_accelerations, onset_ages, sources)
+            metric_reg = - 10 * torch.sum(torch.abs(metric_parameters))
+            regularity = rer_reg
+            # Add a regularization for metrics parameters to avoid overfitting
+            regularity += metric_reg
         if mode == 'complete':
-            regularity += self._compute_class1_priors_regularity()
-            regularity += self._compute_class2_priors_regularity(modulation_matrix)
+            class1_reg = self._compute_class1_priors_regularity()
+            class2_reg = self._compute_class2_priors_regularity(modulation_matrix)
+            regularity += class1_reg
+            regularity += class2_reg
+            print("rer, metric, class1, class2 : ", rer_reg, metric_reg, class1_reg, class2_reg)
         if mode == 'class2':
-            regularity += self._compute_class2_priors_regularity(modulation_matrix)
+            class2_reg = self._compute_class2_priors_regularity(modulation_matrix)
+            regularity += class2_reg
+            print("rer, metric, class2 : ", rer_reg, metric_reg, class2_reg)
 
-        # Add a regularization for metrics parameters to avoid overfitting
-        regularity -= 10 * torch.sum(torch.abs(metric_parameters))
 
         if with_grad:
+            print("Beginning gradient computation : ", time.time())
             total = attachment + regularity
             total.backward(retain_graph=False)
+            print("END gradient computation : ", time.time())
 
             # Gradients of the effects with no closed form update.
             gradient = {}
@@ -279,6 +293,7 @@ class LongitudinalMetricLearning(AbstractStatisticalModel):
                 return attachments.data.cpu().numpy(), gradient
 
         else:
+            print("No gradient used here")
             if mode in ['complete', 'class2']:
                 return attachment.data.cpu().numpy(), regularity.data.cpu().numpy()
             elif mode == 'model':
@@ -449,7 +464,7 @@ class LongitudinalMetricLearning(AbstractStatisticalModel):
             # TODO : find a better way to decide how much attachment we want
             attachments[i] = - 0.5 * torch.sum(residuals[i])
         print(f"Average residuals : {torch.sum(attachments) / number_of_subjects}")
-        return attachments/ noise_variance_torch
+        return attachments/noise_variance_torch
 
     def _compute_absolute_times(self, times, log_accelerations, onset_ages):
         """
@@ -498,7 +513,7 @@ class LongitudinalMetricLearning(AbstractStatisticalModel):
         # Sources random effect
         if sources is not None:
             for i in range(number_of_subjects):
-                regularity -= self.individual_random_effects['sources'].compute_log_likelihood_torch(sources[i], Settings().tensor_scalar_type)
+                regularity += self.individual_random_effects['sources'].compute_log_likelihood_torch(sources[i], Settings().tensor_scalar_type)
 
         # Noise random effect
         regularity -= 0.5 * number_of_subjects * math.log(self.fixed_effects['noise_variance'])