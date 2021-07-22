8211924bb2fe5bffb5972026ee38b67f9f2a2d5e
bsauty <nono.sdc@hotmail.fr>
2021-07-22 17:04:56 +0200

    Change the architecture

--- deformetrica/core/model_tools/neural_networks/networks.py
+++ deformetrica/core/model_tools/neural_networks/networks.py
@@ -8,7 +8,7 @@ from torch.optim import lr_scheduler
 import torchvision
 from torch.utils import data
 
-# This is a dirty workaround a stupid problem with pytorch and osx that mismanage openMP
+# This is a dirty workaround for a stupid problem with pytorch and osx that mismanage openMP
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
@@ -32,34 +32,34 @@ class CAE(nn.Module):
         self.conv3 = nn.Conv3d(8, 8, 3, stride=1, padding=0)
         self.pool1 = nn.MaxPool3d(2, padding=0)
         self.pool2 = nn.MaxPool3d(2, padding=1)
+        self.fc1 = nn.Linear(14784, 600)
+        self.fc2 = nn.Linear(600, 600)
 
         # Decoder
+        self.fc3 = nn.Linear(600, 14784)
         self.up1 = nn.ConvTranspose3d(8, 8, 3, stride=2, padding=1)
         self.up2 = nn.ConvTranspose3d(8, 8, 3, stride=2, padding=0)
         self.up3 = nn.ConvTranspose3d(8, 16, 3, stride=2, padding=1)
         self.conv = nn.Conv3d(16, 1, 4, stride=1, padding=2)
 
     def encoder(self, image):
-        conv1 = self.conv1(self.pad(image))
-        relu1 = F.relu(conv1)
-        pool1 = self.pool1(relu1)
-        conv2 = self.conv2(pool1)
-        relu2 = F.relu(conv2)
-        pool2 = self.pool1(relu2)
-        conv3 = self.conv3(pool2)
-        relu3 = F.relu(conv3)
-        pool3 = self.pool2(relu3)
-        pool3 = pool3.view(pool3.size())
-        return pool3
+        h1 = F.relu(self.conv1(self.pad(image)))
+        h2 = self.pool1(h1)
+        h3 = F.relu(self.conv2(h2))
+        h4 = self.pool1(h3)
+        h5 = F.relu(self.conv3(h4))
+        h6 = self.pool2(h5)
+        h7 = F.relu(self.fc1(h6.flatten(start_dim=1)))
+        h8 = F.relu(self.fc2(h7))
+        h8 = h8.view(h8.size())
+        return h8
 
     def decoder(self, encoded):
-        up1 = self.up1(encoded)
-        up_relu1 = F.relu(up1)
-        up2 = self.up2(up_relu1)
-        up_relu2 = F.relu(up2)
-        up3 = self.up3(up_relu2)
-        up_relu3 = F.relu(up3)
-        reconstructed = self.conv(up_relu3)
+        h9 = F.relu(self.fc3(encoded))
+        h10 = F.relu(self.up1(h9.reshape([encoded.size()[0], 8, 11, 14, 12])))
+        h11 = F.relu(self.up2(h10))
+        h12 = F.relu(self.up3(h11))
+        reconstructed = self.conv(h12)
         reconstructed = torch.sigmoid(reconstructed)
         reconstructed = reconstructed.view(reconstructed.size())
         return reconstructed[:,:,:85,:104,:90]
@@ -69,21 +69,35 @@ class CAE(nn.Module):
         reconstructed = self.decoder(encoded)
         return encoded, reconstructed
 
-    def train(self, data_loader, size, criterion, optimizer, num_epochs=20):
+    def train(self, data_loader, size, criterion, optimizer, num_epochs=20, early_stopping=1e-7):
         print('Start training')
+        best_loss = 1e10
+        early_stopping = 0
+        
         for epoch in range(num_epochs):
+            
+            if early_stopping == 100:
+                break
+
             print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            
             tloss = 0.0
             for data in data_loader:
                 (idx, timepoints), images = data
                 optimizer.zero_grad()
-                input = Variable(images)
-                encoded, reconstructed = self.forward(input)
-                loss = criterion(reconstructed, input)
+                input_ = Variable(images).cuda()
+                encoded, reconstructed = self.forward(input_)
+                loss = criterion(reconstructed, input_)
                 loss.backward()
                 optimizer.step()
                 tloss += float(loss)
             epoch_loss = tloss / size
+
+            if epoch_loss <= best_loss:
+                early_stopping = 0
+                best_loss = epoch_loss
+            else:
+                early_stopping += 1
             print('Epoch loss: {:4f}'.format(epoch_loss))
         print('Complete training')
         return
@@ -164,24 +178,26 @@ class Dataset(data.Dataset):
 
 
 def main():
-    epochs = 100
-    batch_size = 2
+    epochs = 5000
+    es = 1e-6
+    batch_size = 10
     in_dim = 28
-    lr = 0.001
+    lr = 0.01
 
     # Load data
-    train_data = torch.load('/Users/benoit.sautydechalon/deformetrica/deformetrica/LAE_experiments/mini_dataset')
+    train_data = torch.load('../../../LAE_experiments/small_dataset')
+    print(f"Loaded {len(train_data['data'])} MRI scans")
     torch_data = Dataset(train_data['target'], train_data['data'].unsqueeze(1))
     data_loader = torch.utils.data.DataLoader(torch_data, batch_size=batch_size,
                                               shuffle=True, num_workers=4, drop_last=True)
-    autoencoder = CAE()
+    autoencoder = CAE().cuda()
     criterion = nn.BCELoss()
     size = len(train_data)
     optimizer_fn = optim.Adam
     optimizer = optimizer_fn(autoencoder.parameters(), lr=lr)
     autoencoder.train(data_loader, size, criterion, optimizer, num_epochs=epochs)
 
-    test_image = random.choice(train_data['data'])
+    test_image = random.choice(train_data['data']).cuda()
     test_image = Variable(test_image.unsqueeze(0).unsqueeze(0))
     _, out = autoencoder(test_image)
 
